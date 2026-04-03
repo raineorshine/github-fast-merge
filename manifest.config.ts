dae9efb9bedaeef2290c57123dbdf3320ae0929f
Raine Revere <raine@cybersemics.org>
2026-04-03 19:38:40 +0000

    init

---
+++ manifest.config.ts
@@ -0,0 +1,15 @@
+import { defineManifest } from '@crxjs/vite-plugin'
+import pkg from './package.json'
+
+export default defineManifest({
+  manifest_version: 3,
+  name: pkg.name,
+  version: pkg.version,
+  icons: {
+    48: 'public/logo.png',
+  },
+  content_scripts: [{
+    js: ['src/content/main.ts'],
+    matches: ['https://github.com/*/pull/*'],
+  }],
+})
